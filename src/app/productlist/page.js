7f56d22ecede94d79ecea453ff77e834558ff459
“suphanan27” <suphanankonsuay@gmail.com>
2024-08-02 17:05:33 +0700

    frist commit

---
+++ src/app/productlist/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductList from "../../components/ProductsList"
+
+export default function page() {
+  return (
+    <div>
+      <ProductList/>
+    </div>
+  )
+}
